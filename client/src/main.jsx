40c2f9852f04c222291cf7162eb9fe93b96d3641
Shivam77607 <shivamsharma77607@gmail.com>
2024-03-21 20:02:38 +0530

    feat: Add router to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
-import App from "./App.jsx";
 import "./index.css";
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <RouterProvider router={router} />
     </NextUIProvider>
   </React.StrictMode>
 );
