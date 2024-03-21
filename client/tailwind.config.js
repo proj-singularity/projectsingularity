94d51cc12b4978825c2d3dae84ced32e44157d63
Shivam77607 <shivamsharma77607@gmail.com>
2024-03-21 11:50:28 +0530

    Update tailwind.config.js to use dark mode based on media query

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,7 @@ import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "media",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -25,7 +26,6 @@ export default {
       },
     },
   },
-  darkMode: "class",
   plugins: [
     nextui({
       prefix: "nextui", // prefix for themes variables
