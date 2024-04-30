c2921a1c2fd111d958f45c10377402106fa18f1e
Shivam77607 <shivamsharma77607@gmail.com>
2024-04-30 10:47:05 +0530

    Update main.jsx with NextUIProvider and RouterProvider configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <NextUIProvider>
+      <RouterProvider router={router} />
+    </NextUIProvider>
+  </React.StrictMode>
+);
